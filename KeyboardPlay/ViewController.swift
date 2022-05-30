42346b60fe7d07413020efacaec8ae8a1558711c
yongjie_zou <zouyongjie@haixue.com>
2022-05-30 09:32:11 +0800

    Initial Commit

---
+++ KeyboardPlay/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  KeyboardPlay
+//
+//  Created by yongjie on 2022/5/30.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
